3c9cbdc7b2fbdbf21a669b138886d1891904e926
hgncxzy <hgncxzy@qq.com>
2020-01-07 21:03:26 +0800

    fix

---
+++ app/build/generated/source/buildConfig/dev/debug/com/fcbox/ble_test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fcbox.ble_test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.fcbox.ble_test.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "dev";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
